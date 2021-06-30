789188b9a65c6c1df078d025ea8cc2f07eb24832
PHUC MALIK <PHUC MALIK@DESKTOP-0PCO0AU>
2021-06-30 11:44:19 +0700

    Add project files.

---
+++ 1811061441_NguyenHoangPhuc_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811061441_NguyenHoangPhuc_BigSchool.MvcApplication" Language="C#" %>
---
+++ 1811061441_NguyenHoangPhuc_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811061441_NguyenHoangPhuc_BigSchool.Startup))]
+namespace _1811061441_NguyenHoangPhuc_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
